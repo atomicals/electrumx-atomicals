3051d5312cee31fc0f206dfe8f9d90b3acfe8c3e
archx <archx@atomicasdfsdf.org>
2023-07-01 13:05:50 -0500

    fix subscript problem troubleshoot...

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -911,7 +911,7 @@ class BlockProcessor:
             expected_output_indexes = get_expected_output_indexes_of_atomical_ft(mint_info, tx, atomical_id, operations_found_at_inputs) 
         else:
             raise IndexError('color_atomicals_outputs: Unknown type. Index Error.')
-
+        return expected_output_indexes
     # Detect and apply updates-related like operations for an atomical such as mod/evt/crt/sl
     def apply_state_like_updates(operations_found_at_inputs, mint_info, atomical_id, tx_numb, output_idx_le, height):
         if not apply_state_like_updates:
