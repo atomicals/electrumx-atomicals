34076ae90a3d4594798bba62332bd8e92120e83e
archx <archx@atomicasdfsdf.org>
2023-07-01 21:06:49 -0500

    format in mempool

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -749,6 +749,7 @@ class BlockProcessor:
             is_sealed = b'00'
             self.put_atomicals_utxo(mint_info['reveal_location'], mint_info['id'], mint_info['reveal_location_hashX'] + mint_info['reveal_location_scripthash'] + value_sats + is_sealed)
         subtype = mint_info['subtype']
+        atomical_id = mint_info['id']
         self.logger.info(f'Atomicals Create FT in reveal tx {hash_to_hex_str(tx_hash)}, subtype={subtype}, atomical_id={location_id_bytes_to_compact(atomical_id)}, tx_hash={hash_to_hex_str(tx_hash)}')
         return True
 
